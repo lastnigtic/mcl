43afeb203a4807005a39ee672cf4c5ed01ccaec0
scnuyz <472623874@qq.com>
2018-01-28 22:58:37 +0800

    try

--- src/main/java/com/mcl/controller/web/CompController.java
+++ src/main/java/com/mcl/controller/web/CompController.java
@@ -12,7 +12,6 @@ import com.mcl.service.*;
 import com.mcl.util.DateTimeUtil;
 import com.mcl.util.PropertiesUtil;
 import com.mcl.vo.ResumeVO;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
